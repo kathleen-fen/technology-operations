18d81e87fb67478fe4e05862a1974aa6b517cfd2
Kathreen Fenchenko <fev3@mail.ru>
2023-06-24 21:21:24 +0200

    fabrics crud

--- backend/routes/fabrics.js
+++ backend/routes/fabrics.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { getAllFabrics, createFabric } from "./../controllers/fabrics.js";
+import {
+  getAllFabrics,
+  createFabric,
+  updateFabric,
+  deleteFabric,
+} from "./../controllers/fabrics.js";
 
 const router = Router();
 
 router.get("/", getAllFabrics);
 router.post("/add", createFabric);
+router.patch("/update/:id", updateFabric);
+router.delete("/delete/:id", deleteFabric);
 
 export default router;
