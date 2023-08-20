8afb120c83ba804ee076a57e76be6ec66da05f74
Kathreen Fenchenko <fev3@mail.ru>
2023-08-20 16:24:10 +0200

    jest and testing library

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:jest/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh','@typescript-eslint/eslint-plugin'],
+  plugins: ['react-refresh', '@typescript-eslint/eslint-plugin', "jest"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
