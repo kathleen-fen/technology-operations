845cb45758b9e620aa9023dab28726d178ed774e
Kathreen Fenchenko <fev3@mail.ru>
2023-08-20 20:02:06 +0200

    router and error page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import ErrorPage from './error-page.tsx'
 import './index.css'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  errorElement: <ErrorPage />
+}])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
