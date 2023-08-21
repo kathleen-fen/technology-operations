9c57253a171e1e1931ce095e29f984192e79b7f1
Kathreen Fenchenko <fev3@mail.ru>
2023-08-21 19:22:10 +0200

    some refactoring

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import ErrorPage from './error-page.tsx'
 import './index.css'
 
-const router = createBrowserRouter([{
+const routes: RouteObject[] = [{
   path: '/',
   element: <App />,
-  errorElement: <ErrorPage />
-}])
+  errorElement: <ErrorPage/>
+}]
+
+const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
