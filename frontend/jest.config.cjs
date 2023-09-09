84961aa871d6ac966b91ae38957b23c0ef706f56
Kate Fenchenko <kathleen750314@gmail.com>
2023-09-09 21:18:11 +0200

    aliases for jest

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
       '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
         '<rootDir>/__mocks__/fileMock.js',
       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '^components/(.*)$': '<rootDir>/src/components/$1',
+      '^styles/(.*)$': '<rootDir>/src/styles/$1',
     },
     testEnvironment: 'jest-environment-jsdom'
   }
